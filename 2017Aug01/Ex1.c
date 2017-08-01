790247d82dace8fc067d3fbe3d7151c1f26d08f7
Kaushal Kishore <kshr4kshl@gmail.com>
2017-08-01 14:59:07 +0530

    finished: Ex2.c

--- 2017Aug01/Ex1.c
+++ 2017Aug01/Ex1.c
@@ -1,29 +1,31 @@
 #include<stdio.h>
 #include<string.h>
-#include<stdlib.h>
 
+
+/* defining the structure for the student */
 typedef struct student {
     char name[32];
     int id;
 }   student;
 
+
+/* this function will modify the data of the student structure */
 void modify(student *srtptr){
     strcpy(srtptr->name , "abc"  );
     srtptr->id = 123;
 }
 
+/* this function will print the student info */
 void PrintStruc(student *s){
     printf("Name: %s\tID: %d\n", s->name, s->id);
 }
 
 int main(){
-    student *stu1 = (student*)malloc(sizeof(student*));
-    strcpy(stu1->name , "Kaushal Kishore");
-    stu1->id = 111601008;
-    PrintStruc(stu1);
-    modify(stu1);
-    PrintStruc(stu1);
-
-    free(stu1);
+    student stu1 ;
+    strcpy(stu1.name , "Kaushal Kishore");
+    stu1.id = 111601008;
+    PrintStruc(&stu1);
+    modify(&stu1);
+    PrintStruc(&stu1);
     return 0;
 }
--- 2017Aug01/Ex2.c
+++ 2017Aug01/Ex2.c
@@ -1,38 +1,58 @@
+/*                              */
+/*    @kaushal_kishore          */
+/* mailTo: kshr4kshl@gmail.com  */
+/*                              */
+/*         Ex2.c                */
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
-
+/* Defining the student structure */
 typedef struct student {
     char name[32];
     int ID;
 }   student;
 
+/* To print the student INFO */
 void PrintStruc(student *s){
     printf("Name: %s\tID: %d\n", s->name, s->ID);
 }
 
+/* Return 1 if s1->ID is greater than s2->ID */
 int compareStu(student *s1, student *s2){
     return (s1->ID > s2->ID);
 }
 
+/* Entry point */
 int main(int argc, char const *argv[]) {
+    /* N: no. of the students */
     int N;
     scanf("%d " ,&N );
+
+    /* Declaring the student dunamic array */
     student *list =  (student*)malloc(sizeof(student)*N);
+
+    /* maxID structure pointer will point to the student with max ID */
     student *maxID;
-    for (int i=0; i<N; i++){
+
+    scanf("%30[a-zA-Z ] ",list[0].name);
+    scanf(" %d" , &list[0].ID);
+
+    /* Initializing the maxID structure pointer */
+    maxID = &list[0];
+
+    for (int i=1; i<N; i++){
         scanf("%s %d",list[i].name, &list[i].ID);
+        if(compareStu(&list[i] , maxID)) maxID = &list[i];
     }
-/*
+
+/* Uncomment the following code to print all the structural information */
+
     for (int i=0; i<N; i++){
         PrintStruc(&list[i]);
     }
-*/
-    maxID = &list[0];
-    for (int i=1; i<N; i++){
-        if(compareStu(&list[i] , maxID)) maxID = &list[i];
-    }
+
     printf("%s" , maxID->name);
 
     free(list);
