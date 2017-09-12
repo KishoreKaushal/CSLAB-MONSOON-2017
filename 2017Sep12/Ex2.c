bb98703505f724d11ceac8c7c1ef6d4cb7bcae9d
Kaushal Kishore <kshr4kshl@gmail.com>
2017-09-12 21:21:34 +0530

    sorting

---
+++ 2017Sep12/Ex2.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// print array : utility function
+void printArray(int A[], int size){
+    printf("\n");
+    for (int i=0; i<size; i++){
+        printf("%d, ", A[i]);
+    }
+    printf("\n");
+}
+
+// bubble sort algorithm
+void bubbleSort(int arr[] , int size){
+    int temp , flag=0;
+    for (int i=0; i<size; i++){
+        flag=0;
+        for(int j=1; j<size-i ; j++){
+            if(arr[j]>arr[j-1]){		// swap function
+                temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+                flag=1;
+            }
+        }
+        printArray(arr, size);			// print each step
+        if(flag==0) break;				// if there is no swapping
+    }
+}
+
+
+
+int main(){
+    int size =0;
+    scanf("%d" , &size);			// input the size of the array
+    int *ptr = (int*) malloc(sizeof(int)*size) ;	// array containing the data
+	
+    for (int i=0; i<size; i++){			// input the data
+        scanf("%d " , &ptr[i]);
+    }
+    printf("Original Array: ");
+	printArray(ptr , size);			// print the original array
+	printf("\n");		 
+	
+    bubbleSort(ptr , size);			// using the bubble sort algorithm to sort the data
+    printf("\nSORTED ARRAY: ");		
+    printArray(ptr , size);			// print the sorted array	
+    return 0;
+}
---
+++ 2017Sep12/mergeSort.c
@@ -0,0 +1,79 @@
+/*
+	NAME: Kaushal Kishore
+	EMAIL: 111601008@smail.iitpkd.ac.in
+	TOPIC: Merge SORT
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+
+//A: an array
+//p,q,r : indices into A. 
+//Each of the subarray A[p...q] and A[q+1..r] is assumed to be already sorted.
+
+void merge(long long int A[], long long int p, long long int q, long long int r ){
+    long long int n1 = q-p+1 , n2 = r-q;
+    long long int B[n1], C[n2];
+    for (long long int i=0; i<n1 ; i++){
+        B[i] = A[i+p];
+    }
+    for (long long int  j=0; j<n2; j++){
+        C[j] = A[q+1+j];
+    }
+    long long int i, j,k;
+    i=j=0;
+    k=p;
+    while(i<n1 && j<n2){
+        if(B[i]<=C[j])
+            A[k++] = B[i++];
+		else 
+            A[k++] = C[j++];
+    }
+    while(i<n1){
+        A[k++] = B[i++] ;
+    }
+    
+    while(j<n2){
+        A[k++] = C[j++];
+    }
+}
+
+
+// A : input array
+// p,r : starting and ending indices of a subarray of A.
+
+void mergeSort(long long int A[], long long int p, long long int r){
+    if(p<r){
+        long long int q = p+ (r-p)/2;
+        mergeSort(A, p, q);				// recursive call to the function
+        mergeSort(A, q+1, r);
+        merge(A, p, q, r);    			// merging the two part
+    }
+}
+
+
+// UTILITY FUNCTION
+// PRINTING THE ARRAY
+void printArray(long long int a[], long long int size){
+    for(size_t i=0; i<size; i++){
+        printf("\n%lld " , a[i]);
+    }
+    printf("\n");
+}
+
+// MAIN FUNCTION
+int main(){
+    int n;	// ARRAY SIZE
+    scanf("%d" , &n);	// INPUT THE ARRAY SIZE
+    long long int *A = (long long int *) malloc(sizeof(long long int)*n); 	// ALLOCATING THE ARRAY SIZE
+    for(int i=0; i<n; i++){
+        scanf(" %lld" , &A[i]);	// INPUT THE ARRAY
+    }
+
+//    printArray(A,n);	// PRINT THA ARRAY
+    puts("Sorted array");
+    mergeSort(A , 0 , n-1);	// MERGE SORT THE ARRAY
+    printArray(A, n);	// PRINT THE SORTED ARRAY
+    return 0;		// RETURN TO THE SYSTEM
+}
