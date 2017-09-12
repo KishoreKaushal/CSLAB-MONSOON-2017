9e53102a51c33d1a242dc629d43e245a98724022
Kaushal Kishore <kshr4kshl@gmail.com>
2017-09-12 22:03:13 +0530

    Ex4.c

--- 2017Sep12/Ex4.c
+++ 2017Sep12/Ex4.c
@@ -5,25 +5,44 @@
 
 int main(){
     
-    int asc=0 , desc=0 ;
-
+    int i,j, k, asc=0 , desc=0 ;
+    int err=0;
     int r, c;
     printf("row: ");
     scanf("%d\n" , &r);
     printf("col: ");
     scanf("%d\n" , &c);
 
-    int **ptr = (int *) malloc(sizeof(int)*r);
+    int **ptr = (int **) malloc(sizeof(int)*r);
 
-
-    for (int i=0; i<r; i++){
-        ptr[i] = (int *)malloc(sizeof(int)*c);
-        for (int j=0; j<c; j++){
-            scanf(" %d", &ptr[i][j]);
+    ptr[0] = (int*) malloc(sizeof(int)*c);
+    scanf(" %d", &ptr[0][0]);
+    for (int j=1; j<c; j++){
+        scanf(" %d" , &ptr[0][j]);
+        if((ptr[0][j-1]<ptr[0][j] && desc==1) || (ptr[0][j-1]>ptr[0][j] && asc==1)){
+            err= 1;
+        } else if(ptr[0][j-1]<ptr[0][j]) {
+            asc=1;
+        } else if(ptr[0][j-1]>ptr[0][j]) {
+            desc=1;
         }
     }
+    
+    if(err != 1){
 
-    for (int j=0; j<c ; j++){
+        for (i=1; i<r; i++){
+            ptr[i] = (int *)malloc(sizeof(int)*c);
+            for (j=0; j<c; j++){
+                scanf(" %d", &ptr[i][j]);
+            }
+        }   
+    }
+    if(err==1) {
+        // pass //
+        return 2;
+    }
+
+    for (j=0; j<c ; j++){
             free(ptr[j]);
     }
     free(ptr);
