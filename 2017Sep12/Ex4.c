2b5ecde4fee444549dd871c16e7d8f8b7c9e1578
Kaushal Kishore <kshr4kshl@gmail.com>
2017-09-13 09:12:29 +0530

    Ex4.c

--- 2017Sep12/Ex4.c
+++ 2017Sep12/Ex4.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-
+void print2Darray(int **a , int r, int c){
+    printf("YOUR MATRIX: \n");
+    for (int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            printf("%3d " , a[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 int main(){
     
@@ -9,18 +17,22 @@ int main(){
     int err=0;
     int r, c;
     printf("row: ");
-    scanf("%d\n" , &r);
+    scanf(" %d" , &r);
     printf("col: ");
-    scanf("%d\n" , &c);
+    scanf(" %d" , &c);
 
-    int **ptr = (int **) malloc(sizeof(int)*r);
+    int **ptr = (int **) malloc(sizeof(int*)*r);
 
-    ptr[0] = (int*) malloc(sizeof(int)*c);
+    ptr[0] = (int *) malloc(sizeof(int)*c);
+    // ptr[1] = (int *) malloc(sizeof(int)*c);
+
+    printf("Enter the matrix: \n");
     scanf(" %d", &ptr[0][0]);
     for (int j=1; j<c; j++){
         scanf(" %d" , &ptr[0][j]);
         if((ptr[0][j-1]<ptr[0][j] && desc==1) || (ptr[0][j-1]>ptr[0][j] && asc==1)){
             err= 1;
+            break;
         } else if(ptr[0][j-1]<ptr[0][j]) {
             asc=1;
         } else if(ptr[0][j-1]>ptr[0][j]) {
@@ -28,22 +40,41 @@ int main(){
         }
     }
     
-    if(err != 1){
-
+    if(!err){
+  
         for (i=1; i<r; i++){
             ptr[i] = (int *)malloc(sizeof(int)*c);
-            for (j=0; j<c; j++){
-                scanf(" %d", &ptr[i][j]);
+            scanf(" %d" , &ptr[i][0]);
+            if ((asc && ( ptr[i-1][0]>ptr[i][0])) || (desc && ( ptr[i-1][0]<ptr[i][0] ))) {
+                err = 1;
+                break;
             }
+
+            for (j=1; j<c; j++){
+                scanf(" %d", &ptr[i][j]);
+                if( (asc && ( (ptr[i][j-1] > ptr[i][j]) || (ptr[i-1][j] > ptr[i][j] ) ) ) 
+                        ||  (desc && ( (ptr[i][j-1] < ptr[i][j] ) || ( ptr[i-1][j] < ptr[i][j])))) {
+                    err = 1;
+                    break;
+                }
+            }
+            if(err==1) break;
         }   
     }
-    if(err==1) {
-        // pass //
-        return 2;
+    
+    /* priniting the matrix if the order is correct */ 
+    if(!err) {
+        print2Darray(ptr , r, c);   
+        int n;
+        /* required operation will take place */ 
+        printf("Enter a number you want to search for: ");
+        scanf("%d", &n);
+        searchElement(ptr , r, c, n);
     }
+    else puts("ERROR: Matrix is not in the correct order. Must be either decreasing or increasing.");
 
-    for (j=0; j<c ; j++){
-            free(ptr[j]);
+    for (i=0; i<r ; i++){
+            if(ptr[i]) free(ptr[i]);
     }
     free(ptr);
     return 0;
